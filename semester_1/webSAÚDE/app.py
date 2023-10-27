d91a5a26dd67351849d9e35383aa922b60a6e7bd
Victor Godoy <victor.hgodoy@icloud.com>
2023-10-27 09:23:41 -0300

    update

--- semester_1/webSAÚDE/app.py
+++ semester_1/webSAÚDE/app.py
@@ -7,7 +7,7 @@ def index():
 
 @app.route('/imc')
 def imc():
-     return render_template('imx.html')
+     return render_template('imc.html')
 
 @app.route('/atividade')
 def atividade():
@@ -17,9 +17,32 @@ def atividade():
 def agua():
      return render_template('agua.html')
 
-if __name__ == '__main__':
-     app.run(debug=True)
 
+@app.route('/calcular', methods=['POST'])
+def calcular():
+    peso = float(request.form['peso'])
+    altura = float(request.form['altura'])
+    imc = peso / (altura ** 2) 
+
+    if imc < 16.9:
+        resultado = 'Muito abaixo do peso!'
+    elif imc <= 18.4:
+        resultado = 'Abaixo do peso!'
+    elif imc <= 24.9:
+        resultado = 'Peso Normal!'
+    elif imc <= 29.9:
+        resultado = 'Acima do peso!'
+    elif imc <= 34.9:
+        resultado = 'Obesidade grau I'
+    elif imc <= 40:
+        resultado = 'Obesidade grau II'
+    else:
+        resultado = 'Obesidade grau III'
+
+    return render_template('imc.html', resultado=resultado)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
