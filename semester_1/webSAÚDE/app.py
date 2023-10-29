e146e2ce68a979a081cc41c767ad176bdad32cbe
Victor Godoy <victor.hgodoy@icloud.com>
2023-10-29 10:10:32 -0300

    update

--- semester_1/webSAÚDE/app.py
+++ semester_1/webSAÚDE/app.py
@@ -17,7 +17,7 @@ def atividade():
 def agua():
      return render_template('agua.html')
 
-
+# IMC
 @app.route('/calcular', methods=['POST'])
 def calcular():
     peso = float(request.form['peso'])
@@ -25,13 +25,13 @@ def calcular():
     imc = peso / (altura ** 2) 
 
     if imc < 16.9:
-        resultado = 'Muito abaixo do peso!'
+        resultado = 'Muito abaixo do peso !'
     elif imc <= 18.4:
-        resultado = 'Abaixo do peso!'
+        resultado = 'Abaixo do peso !'
     elif imc <= 24.9:
-        resultado = 'Peso Normal!'
+        resultado = 'Peso Normal'
     elif imc <= 29.9:
-        resultado = 'Acima do peso!'
+        resultado = 'Acima do peso !'
     elif imc <= 34.9:
         resultado = 'Obesidade grau I'
     elif imc <= 40:
@@ -41,29 +41,19 @@ def calcular():
 
     return render_template('imc.html', resultado=resultado)
 
+
+# ÁGUA
+@app.route('/calcular_agua', methods=['POST'])
+def calcular_agua():
+    peso = float(request.form['peso'])
+    agua = peso * 35  
+
+    return render_template('agua.html', resultado=agua)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-
-
-# peso_kg = float(input("Digite o seu peso (kg): "))
-# altura = float(input("Digite a sua altura (m): "))
-# imc = (peso_kg) / (altura * 2) 
-# if imc < 16.9:
-#     print(f'Seu IMC é {imc}.Muito abaixo do peso!')
-# elif 17 <= imc <= 18.4:
-#     print(f'Seu IMC é {imc}.Abaixo do peso!')
-# elif 18.5 <= imc <= 24.9:
-#     print(f'Seu IMC é {imc}.Peso Normal, Parabéns!') 
-# elif 25 <=  imc <= 29.9:
-#      print(f'Seu IMC é {imc}.Acima do peso!') 
-# elif 30 <=  imc <= 34.9:
-#      print(f'Seu IMC é {imc}.Obesidade grau I') 
-# elif 35 <=  imc <= 40:
-#      print(f'Seu IMC é {imc}.Obesidade grau II') 
-# else:
-#      print(f'Seu IMC é {imc}.Obesidade grau III')
 
 
  
